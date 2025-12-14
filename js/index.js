f7bb716fb0d97b333c1433bde591aa50bc2c4291
nixon-crypto <leandro.souza2017@hotmail.com>
2025-12-13 23:49:56 -0300

    carrossel

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// diretorio das imagens dentro de um array
 const imagens = [
   "/img/img1.png",
   "/img/img2.png",
@@ -5,22 +6,36 @@ const imagens = [
   "/img/img4.png",
 ];
 
-let imgAtual = 0;
+let imgAtual = 0;  // contador para as imagens
 
-const img = document.querySelector(".slide img");
+const img = document.getElementById("imagem");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
 
-document.getElementById("next").onclick = () =>{
-    imgAtual++;
+// botao para passar para a proxima imagem
+next.addEventListener("click", () =>{
+    imgAtual++; //ir para a proxima imagem
     if(imgAtual >= imagens.length){
         imgAtual = 0;
     }
-    img.src = imagens[imgAtual];
-};
-document.getElementById("prev").onclick = () =>{
-    imgAtual--;
-    if(imgAtual < 0){
-        imgAtual = imagens.length - 1;
-    }
-    img.src = imagens[imgAtual];
-};
+    trocarImagem();
+});
 
+// botao para voltar para imagem anterior
+prev.addEventListener("click", () =>{
+    imgAtual--; //voltar para a proxima imagem
+    if(imgAtual < 0){
+        imgAtual = imagens.length - 1; // para deixar em loop
+    }
+    trocarImagem();
+});
+
+// efeito de transição de imagem
+function trocarImagem(){
+    img.style.opacity = 0;
+
+    setTimeout(() =>{
+        img.src = imagens[imgAtual];
+        img.style.opacity = 1;
+    },200);
+};
