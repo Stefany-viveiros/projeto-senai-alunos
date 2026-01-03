3b16674cb02e264cf3413f48104e0a940ddc15d0
Stefany viveiros <viveirosstefany@gmail.com>
2026-01-02 21:15:34 -0300

    Criando estrutura inicial flask para o back-end

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/carteirinha")
+def carteirinha():
+    return render_template("carteirinha.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
